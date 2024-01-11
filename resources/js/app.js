6cbe51bfdb183976a2bc36f6bfa88ff35e6afae6
emi09 <e.kokoa0928@gmail.com>
2024-01-11 23:21:40 +0900

    app.js修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,19 @@ import { createApp } from 'vue';
  * to use in your application's views. An example is included for you.
  */
 
-const app = createApp({});
+// const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0],files(key).default))
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+  el: '#app',
+  });
