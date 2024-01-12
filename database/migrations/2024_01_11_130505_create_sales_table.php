de20e91556a55792582d4ff9be36625cbda5faee
emi09 <e.kokoa0928@gmail.com>
2024-01-12 22:56:57 +0900

    DB-sails修正

--- database/migrations/2024_01_11_130505_create_sales_table.php
+++ database/migrations/2024_01_11_130505_create_sales_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->integer('product_id');
+            $table->foreignId('product_id')->constrained('products');
             $table->timestamps();
         });
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,61 @@
-import './bootstrap';
-import { createApp } from 'vue';
-
 /**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
-
-// const app = createApp({});
-
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+* First we will load all of this project's JavaScript dependencies which
+* includes Vue and other libraries. It is a great starting point when
+* building robust, powerful web applications using Vue and Laravel.
+*/
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
+/**
+* The following block of code may be used to automatically register your
+* Vue components. It will recursively scan this directory for the Vue
+* components and automatically register them with their "basename".
+*
+* Eg. ./components/ExampleComponent.vue ->
+*/
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0],files(key).default))
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+* Next, we will create a fresh Vue application instance and attach it to
+* the page. Then, you may begin adding components to this application
+* or customize the JavaScript scaffolding to fit your unique needs.
+*/
 
 const app = new Vue({
-  el: '#app',
-  });
+el: '#app',
+});
+
+
+
+
+
+
+// import './bootstrap';
+// import { createApp } from 'vue';
+
+// /**
+//  * Next, we will create a fresh Vue application instance. You may then begin
+//  * registering components with the application instance so they are ready
+//  * to use in your application's views. An example is included for you.
+//  */
+
+// // const app = createApp({});
+
+// import ExampleComponent from './components/ExampleComponent.vue';
+// app.component('example-component', ExampleComponent);
+
+// require('./bootstrap');
+
+// window.Vue = require('vue');
+
+// // const files = require.context('./', true, /\.vue$/i)
+// // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0],files(key).default))
+// // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// const app = new Vue({
+//   el: '#app',
+//   });
