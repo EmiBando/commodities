8b3caac7ca575e852c7d0dc9b41dcc43399a0af0
emi09 <e.kokoa0928@gmail.com>
2024-01-18 10:33:18 +0900

    registerController編集

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
-        $this->redirectTo = route('post.list');
+        $this->redirectTo = route('list');
     }
 
     /**
